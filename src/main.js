f74c5eb6077915e5c4de9fed8c05794dadce7f7d
leeseean chiu <leeseean@qq.com>
2017-07-22 21:04:22 +0800

    添加axios库添加vuex

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'babel-polyfill'
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App'
 import router from './router'
 
@@ -9,9 +11,14 @@ import 'iview/dist/styles/iview.css'
 
 import less from 'less'
 
+import axios from 'axios'
+
 Vue.config.productionTip = false
 Vue.use(iView)
 Vue.use(less)
+Vue.use(Vuex)
+Vue.prototype.$http = axios
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
