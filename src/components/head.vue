6abab0f25082750d92f9c32a9613eaec78e40ab1
leeseean chiu <leeseean@qq.com>
2017-07-26 01:34:30 +0800

    仿站头部

---
+++ src/components/head.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="headTop clearfix">
+    <div class="headTopLeft">网贷羊毛QQ交流群：227322790</div>
+    <div class="headTopRight">
+      <span class="marqueenTip">友情提示：</span>
+      <span class="marqueenWrap inline-block">
+        <span class="headMarqueeContent inline-block">网贷有风险，撸毛要谨慎！无论多么牛逼的平台，都有跑路倒闭的风险！请且撸且珍惜。如果您还没有做好踩雷中弹的准备，请直接关闭本站！感谢您对网贷羊毛的关注和支持！</span>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang="less" scoped>
+ @import "../../static/less/head.less";
+</style>
