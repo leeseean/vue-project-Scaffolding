37b1db4dab78e6b2f3e4274b9fc0c92972f5a492
leeseean <leeseean@qq.com>
2017-09-29 13:36:29 +0800

    00

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,21 @@
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  export default {
+    name: 'app'
+  }
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  width: 1260px;
-  margin: auto;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    width: 1260px;
+    margin: auto;
+  }
+
 </style>
